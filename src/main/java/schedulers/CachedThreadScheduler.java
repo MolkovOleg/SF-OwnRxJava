d31133572daba9749e55cbee50f2eb3ad1728ebc
HP OMEN <ol.molkov@mail.ru>
2025-06-02 16:15:48 +0300

    feat: Добавлен менеджер кэширующих пул потокв CachedThreadScheduler

---
+++ src/main/java/schedulers/CachedThreadScheduler.java
@@ -0,0 +1,20 @@
+package schedulers;
+
+import components.Scheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadScheduler implements Scheduler {
+
+    private final ExecutorService executor;
+
+    public CachedThreadScheduler(ExecutorService executor) {
+        this.executor = Executors.newCachedThreadPool();
+    }
+
+    @Override
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+}
